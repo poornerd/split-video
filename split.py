ed9ead1552a35518bfaaea5f2f3ae314ad2456d0
Brian Porter <brian@porter.net>
2022-10-22 09:31:32 +0200

    new version for creating screenshots for positioning highlight arrows

--- split.py
+++ split.py
@@ -15,8 +15,10 @@ def cleanup():
         os.remove(f);
     for f in glob.glob("tmp_part*.mov"):
         os.remove(f);
-    for f in glob.glob("tmp_part*.JPG"):
-        os.remove(f);          
+    for f in glob.glob("part*.JPG"):
+        os.remove(f);  
+    for f in glob.glob("part*.jpg"):
+        os.remove(f);                   
 
 # split files
 def split(fname):
@@ -27,19 +29,34 @@ def split(fname):
             line_count = line_count + 1;
             print(words);
             output_filename = "part_" + str(line_count).zfill(4) + "_" + words[3] + ".MOV";
+            highlight_filename = "part_" + str(line_count).zfill(4) + "_" + words[3] + ".jpg";
             if not os.path.exists(output_filename) :
                 if words[4] and words[5]:
-                    arrow_cmd = ["-vf", "drawtext=text='^':enable='between(t,0,.3)':x=" + words[4] + ":y=" + words[5] + ":fontsize=66:fontcolor=red,loop=40:1"];
+                    arrow_cmd = ["-vf", "drawtext=text='^':enable='between(t,0,.2)':x=" + words[4] + ":y=" + words[5] + ":fontsize=166:fontcolor=red,loop=40:1"];
                 else:
                     arrow_cmd = ["-vf", "loop=40:1"];
                 split_cmd = ["ffmpeg", "-ss", words[1],  "-i", words[0], "-t", words[2], "-c:v", "libx264", "-s", "1920x1080"];
                 output_cmd = [ output_filename];
                 # split_cmd.append(arrow_cmd);
                 subprocess.check_output(split_cmd + arrow_cmd + output_cmd);
-                # highlight_cmd = ["ffmpeg", "-y", "-i", output_filename, "-vf", "drawtext=text='^':x=1140:y=260:fontsize=66:fontcolor=red", "-c:a", "copy", "O_" + output_filename];
-                # subprocess.check_output(highlight_cmd);
+
+                # replicate final re-encode by adding to the split? 
+                # and skip this encode?
                 reencode_cmd = ["ffmpeg", "-i", output_filename, "-c", "copy", "-bsf:v", "h264_mp4toannexb", "-f",  "mpegts", "tmp_" + output_filename];
                 subprocess.check_output(reencode_cmd);
+
+                x= 960;
+                if words[4]:
+                    x = int(words[4]);
+                y = 540;
+                if words[5]:    
+                    y = int(words[5]);
+                highlight_cmd = ["ffmpeg", "-y", "-i", "tmp_" + output_filename, "-vf", "drawtext=text='"+str(x) + ","+str(y) +"':x=" + str(x + 25) + ":y=" + str(y + 50) + ":fontsize=24:fontcolor=red", "-c:a", "copy", "tmp2_" + output_filename];
+                subprocess.check_output(highlight_cmd);
+
+                extract_img_cmd = ["ffmpeg",  "-i",  "tmp2_" + output_filename,  "-ss",  "00:00", "-vframes", "1",  highlight_filename];
+                subprocess.check_output(extract_img_cmd);
+                os.remove("tmp2_" + output_filename); 
 
 # create the highlight file
 def create_highlight_film():
@@ -75,15 +92,15 @@ def create_highlight_film(name):
     for f in glob.glob(filmname):
         os.remove(f);
     file1 = open("mylist_" + name + ".txt", "w"); 
-    files = sorted(list(glob.glob("tmp_part*"+name+"*.MOV"))+ list(glob.glob("tmp_part*"+name+"*.mov")));
+    files = sorted(list(glob.glob("part*"+name+"*.MOV"))+ list(glob.glob("part*"+name+"*.mov")));
     for item in files:
         file1.write("file %s\n" %item);
         # if item.endswith(".JPG") :
         #    file1.write("duration 1\n");   
     file1.close();
 
-    goals_cmd = ["ffmpeg", "-f" , "concat", "-safe", "0", "-i", "mylist_"+name+".txt", "-bsf:a", "aac_adtstoasc", "-fflags", "+genpts", "-c", "copy", filmname];
-    subprocess.check_output(goals_cmd);
+    highlight_cmd = ["ffmpeg", "-f" , "concat", "-safe", "0", "-i", "mylist_"+name+".txt", "-bsf:a", "aac_adtstoasc", "-fflags", "+genpts", "-c", "copy", filmname];
+    subprocess.check_output(highlight_cmd);
 
     reencode_cmd = ["ffmpeg", "-y", "-i", filmname, "-c:v", "libx264", "-preset", "slow", "-crf", "20", "-c:a", "aac", "-b:a", "160k", "-vf", "format=yuv420p", "-movflags", "+faststart", "final_" + filmname];
     subprocess.check_output(reencode_cmd);
